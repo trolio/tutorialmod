4777ec13d59eed593d956d1139874efd76c4f417
trolio <42102227+trolio@users.noreply.github.com>
2018-08-07 16:22:39 -0400

    episode 2

---
+++ src/main/java/trolio/tutorialmod/proxy/ClientProxy.java
@@ -0,0 +1,6 @@
+package trolio.tutorialmod.proxy;
+
+public class ClientProxy extends CommonProxy
+{
+	
+}
---
+++ src/main/java/trolio/tutorialmod/util/Reference.java
@@ -0,0 +1,11 @@
+package trolio.tutorialmod.util;
+
+public class Reference 
+{
+	public static final String MODID = "tm";
+	public static final String NAME = "Tutorial Mod";
+	public static final String VERSION = "0.0.1";
+	
+	public static final String CLIENT = "trolio.tutorialmod.proxy.ClientProxy";
+	public static final String SERVER = "trolio.tutorialmod.proxy.CommonProxy";
+}
